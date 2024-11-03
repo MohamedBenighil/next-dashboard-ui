c7945325cd8f41b349a4c059ce4c5e35b16aa5a8
Mohamed B. <mohamed.benighil07@gmail.com>
2024-11-03 11:15:32 +0100

    07. React Next.js Animated Line Chart Example (part2)

--- src/app/components/FinanceChart.tsx
+++ src/app/components/FinanceChart.tsx
@@ -13,45 +13,63 @@ import {
 
 const data = [
   {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
+    name: "Jan",
+    expense: 4000,
+    income: 2400,
     amt: 2400,
   },
   {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
+    name: "Feb",
+    expense: 3000,
+    income: 1398,
     amt: 2210,
   },
   {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
+    name: "Mar",
+    expense: 2000,
+    income: 9800,
     amt: 2290,
   },
   {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
+    name: "Apr",
+    expense: 2780,
+    income: 3908,
     amt: 2000,
   },
   {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
+    name: "May",
+    expense: 1890,
+    income: 4800,
     amt: 2181,
   },
   {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
+    name: "Jun",
+    expense: 2390,
+    income: 3800,
     amt: 2500,
   },
   {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
+    name: "Aug",
+    expense: 3490,
+    income: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Sep",
+    expense: 3490,
+    income: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Nov",
+    expense: 3490,
+    income: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Dec",
+    expense: 3490,
+    income: 4300,
     amt: 2100,
   },
 ];
@@ -76,18 +94,39 @@ const FinanceChart = () => {
             bottom: 5,
           }}
         >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" stroke="#ddd" />
+          <XAxis
+            dataKey="name"
+            axisLine={false}
+            tick={{ fill: "#d1d7db" }}
+            tickLine={false}
+            tickMargin={10}
+          />
+
+          <YAxis
+            axisLine={false}
+            tick={{ fill: "#d1d7db" }}
+            tickLine={false}
+            tickMargin={20}
+          />
           <Tooltip />
-          <Legend />
+          <Legend
+            align="center"
+            verticalAlign="top"
+            wrapperStyle={{ paddingBottom: "10px", paddingTop: "30px" }}
+          />
           <Line
             type="monotone"
-            dataKey="pv"
+            dataKey="income"
             stroke="#8884d8"
-            activeDot={{ r: 8 }}
+            strokeWidth={5}
           />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          <Line
+            type="monotone"
+            dataKey="expense"
+            stroke="#82ca9d"
+            strokeWidth={5}
+          />
         </LineChart>
       </ResponsiveContainer>
     </div>
